49487d57371fcfba1194d9bf121c39906fd49146
ypablo <yfedora@gmail.com>
2019-07-20 01:24:53 +0100

    project

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,31 @@
 import React from "react";
+import style from "./style.css";
 
-function Footer() {
-    return (
-        <div>
-            <p>footer</p>
-        </div>
-    )
+class Footer extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                isLoggedIn: !prevState.isLoggedIn
+            }
+        })
+    }
+
+    render() {
+        let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN";
+        return (
+            <div className="footer">
+                <button onClick={this.handleClick}>{buttonText}</button>
+            </div>
+        )
+    }
 }
 
 

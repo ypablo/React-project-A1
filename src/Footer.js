8d1942ce6da8ef8eb41c2cd93e8d3237a56231ec
ypablo <yfedora@gmail.com>
2019-07-20 02:20:12 +0100

    project

--- src/Footer.js
+++ src/Footer.js
@@ -20,9 +20,11 @@ class Footer extends React.Component {
 
     render() {
         let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN";
+        let displayText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN";
         return (
             <div className="footer">
                 <button onClick={this.handleClick}>{buttonText}</button>
+                <h1>{displayText}</h1>
             </div>
         )
     }
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -3,11 +3,16 @@ import style from "./style.css";
 import todosData from "./todosData";
 
 function ToDoItem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
 
     return (
         <div className="todo-item">
             <input onChange={() => props.handleChange(props.item.id)} type="checkbox" checked={props.item.completed} />
-            <p>{props.item.text}</p>
+            <p style={props.item.completed ? completedStyle : null}>{props.item.text}</p>
         </div>
     );
 }
